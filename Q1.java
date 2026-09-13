f444d202981fd0c0c24dc028b25dc21cff1451eb
Tanya <tanya@Tanyas-MacBook-Air-3.local>
2026-09-13 11:50:34 +0530

    Day-1_Q1

---
+++ Q1.java
@@ -0,0 +1,16 @@
+// Q1 (User Inputs, Operations & Output)
+// Write a program to input two numbers and display their sum.
+
+import java.util.*;
+
+class Main {
+        public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int n1 = sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int n2 = sc.nextInt();
+        int sum = n1+n2;
+        System.out.println(sum);
+    }
+}
