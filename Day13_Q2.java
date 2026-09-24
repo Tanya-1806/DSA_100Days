9d00015d436dda2e5c882fd11f13e048fed4270a
Tanya <tmarwaha1806@gmail.com>
2026-09-24 10:45:31 +0530

    Day-13

---
+++ Day13_Q2.java
@@ -0,0 +1,18 @@
+// Q26 (Loops without Arrays/Strings)
+// Write a program to print numbers from 1 to n.
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Numbers from 1 to " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
